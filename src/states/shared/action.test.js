d422bb2a11812bfdf14c18c5a8fee86f35dfb200
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-03 22:23:22 +0700

    automation testing

---
+++ src/states/shared/action.test.js
@@ -0,0 +1,7 @@
+/**
+ * skenario test
+ *
+ * - asyncPopulateUsersAndTalks thunk
+ *  - should dispatch action correctly when data fetching success
+ *  - should dispatch action and call alert correctly when data fetching failed
+ */
